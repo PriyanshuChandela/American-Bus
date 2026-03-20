14a874ec4756d245841b01de17f4f56f597674c1
Priyanshu Chandela <priyanshuchandela84@gmail.com>
2026-03-20 17:18:56 +0530

    Images

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
       {/* BACKGROUND */}
       <div className="absolute inset-0">
         <img
-          src="/src/assets/hero-background.png"
+          src="/public/images/hero-background.png"
           alt=""
           className="w-full h-full object-cover"
         />
@@ -75,7 +75,7 @@ const Hero = () => {
         <div className="w-[45%] h-[250px] sm:h-[350px] md:h-[500px] relative">
 
           <img
-            src="/src/assets/hero-image.png"
+            src="/public/images/hero-image.png"
             alt=""
             className="w-full h-full object-cover rounded-xl sm:rounded-2xl"
           />
